7a3c356425b8fa002a52113cf055f75a4a0d0bdf
ST-Alan <contacto@alanfermin.com>
2024-03-27 11:58:45 -0500

    Instrucciones para desplegar a produccion

--- src/presentation/router/router.tsx
+++ src/presentation/router/router.tsx
@@ -5,77 +5,77 @@ import { DashboardLayout } from "../layouts/DashboardLayout";
 
 export const menuRoutes = [
     {
-      to: "/orthography",
+      to: "react/orthography",
       icon: "fa-solid fa-spell-check",
       title: "Ortografía",
       description: "Corregir ortografía",
       component: <OrthographyPage />
     },
     {
-      to: "/pros-cons",
+      to: "react/pros-cons",
       icon: "fa-solid fa-code-compare",
       title: "Pros & Cons",
       description: "Comparar pros y contras",
       component: <ProsConsPage />
     },
     {
-      to: "/pros-cons-stream",
+      to: "react/pros-cons-stream",
       icon: "fa-solid fa-water",
       title: "Como stream",
       description: "Con stream de mensajes",
       component: <ProsConsStreamPage />
     },
     {
-      to: "/translate",
+      to: "react/translate",
       icon: "fa-solid fa-language",
       title: "Traducir",
       description: "Textos a otros idiomas",
       component: <TranslatePage />
     },
     {
-      to: "/translate-stream",
+      to: "react/translate-stream",
       icon: "fa-solid fa-dumbbell",
       title: "Traducir Stream",
       description: "Textos a otros idiomas",
       component: <TranslateStreamPage />
     },
     {
-      to: "/translate-stream-function",
+      to: "react/translate-stream-function",
       icon: "fa-solid fa-user-astronaut",
       title: "Traducir con Stream y Funcion Generadora",
       description: "Textos a otros idiomas",
       component: <TranslateStreamGeneratorPage />
     },
     {
-      to: "/text-to-audio",
+      to: "react/text-to-audio",
       icon: "fa-solid fa-podcast",
       title: "Texto a audio",
       description: "Convertir texto a audio",
       component: <TextToAudioPage />
     },
     {
-      to: "/audio-to-text",
+      to: "react/audio-to-text",
       icon: "fa-solid fa-comment-dots",
       title: "Audio a texto",
       description: "Convertir audio a texto",
       component: <AudioToTextPage />
     },
     {
-      to: "/image-generation",
+      to: "react/image-generation",
       icon: "fa-solid fa-image",
       title: "Imágenes",
       description: "Generar imágenes",
       component: <ImageGenerationPage />
     },
     {
-      to: "/image-tunning",
+      to: "react/image-tunning",
       icon: "fa-solid fa-wand-magic",
       title: "Editar imagen",
       description: "Generación continua",
       component: <ImageTunningPage />
     },
     {
-      to: "/assistant",
+      to: "react/assistant",
       icon: "fa-solid fa-user",
       title: "Asistente",
       description: "Información del asistente",
